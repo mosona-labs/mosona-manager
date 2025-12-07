9db6a376bbe430243af64729a79decc88fb100a8
Arsfy <arsfy@outlook.com>
2025-12-07 17:50:32 +0800

    feat: implement auto-renew functionality for server subscriptions

--- task/init.go
+++ task/init.go
@@ -4,6 +4,7 @@ import "time"
 
 func Init() {
 	go runTask(SystemUsage, 10*time.Second) // 10 seconds
+	go runTask(AutoRenew, 1*time.Hour)      // 1 hour
 }
 
 func runTask(fn func(), duration time.Duration) {
